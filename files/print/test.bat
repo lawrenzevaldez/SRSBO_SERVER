536795a02dabc51c951c41f688116359c17ae8c4
lawrenzevaldez <valdezlawrenze@gmail.com>
2025-07-30 13:28:47 +0800

    Migrate fixex

--- files/print/test.bat
+++ files/print/test.bat
@@ -1,6 +1,2 @@
 RUNDLL32 PRINTUI.DLL,PrintUIEntry /y /n "%EPSONTM%" 
-<<<<<<< HEAD
 start /min notepad /P C:\Users\Lawrenze\Desktop\DESKTOP\BO FOR DEPLOYMENT\SRSBO_SERVER\files\print\print_test.txt
-=======
-start /min notepad /P C:\Users\ISD JL\Desktop\BO FOR DEPLOYMENT\OLD BO\SRSBO_SERVER\files\print\print_test.txt
->>>>>>> 5dd32781f3b418bce286071aada135c83bd6f548
